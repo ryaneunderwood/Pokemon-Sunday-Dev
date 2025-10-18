dcd255de8dd7eab1269413da32c1104d7490561a
ryaneunderwood <rucranberry@gmail.com>
2025-10-17 19:54:47 -0700

    first ZA sprites and resize.
    first ZA sprites and resize.

--- resizeSprites.py
+++ resizeSprites.py
@@ -1,7 +1,7 @@
 from PIL import Image,ImageFile
 import os
 directory='Graphics/Battlers/'
-folder=['./']#['Back/','BackShiny/','Front/','FrontShiny/','Back/Female/','BackShiny/Female/','Front/Female/','FrontShiny/Female/']
+folder=['Back/','BackShiny/','Front/','FrontShiny/','Back/Female/','BackShiny/Female/','Front/Female/','FrontShiny/Female/']
 for f in folder:
     ImageFile.LOAD_TRUNCATED_IMAGES = True
     for filename in os.listdir(directory+f):
@@ -9,9 +9,9 @@ for f in folder:
             try:
                 image=Image.open(directory+f+filename)
                 #print(directory+filename,' 2')
-                if image.size[0]==288:
+                if image.size[0]==96:
                     print(f+filename)
-                    new_image=image.resize((int(0.6667*image.size[0]),int(0.6667*image.size[1])), Image.NEAREST)
+                    new_image=image.resize((int(2*image.size[0]),int(2*image.size[1])), Image.NEAREST)
                     print('a')
                     #new_image=new_image.resize((2*new_image.size[0],2*new_image.size[1]), Image.NEAREST)
                     new_image.save(directory+f+filename)
